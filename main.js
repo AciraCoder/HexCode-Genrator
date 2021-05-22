19db78e37c29e62560d18f231c0940797fa153e5
AciraCoder <76238914+AciraCoder@users.noreply.github.com>
2021-05-22 11:29:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+const pelette1 = document.getElementById('pelette1')
+const pelette2 = document.getElementById('pelette2')
+
 let hexColor = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'
 ]
 
-const colorDisplay = document.getElementById('color')
+const colorDisplay1 = document.getElementById('color1')
+const colorDisplay2 = document.getElementById('color2')
 
 window.addEventListener('keydown', e => {
     if (e.keyCode == 32) {
@@ -10,8 +14,22 @@ window.addEventListener('keydown', e => {
         for (i = 0; i < 6; i++) {
             let randomColor = Math.floor(Math.random() * hexColor.length);
             hex += hexColor[randomColor];
-            document.body.style.backgroundColor = hex;
-            colorDisplay.innerHTML = hex;
+            pelette1.style.backgroundColor = hex;
+            pelette2.style.backgroundColor = hex;
+            colorDisplay1.innerHTML = hex;
+            colorDisplay2.innerHTML = hex;
+        }
+    }
+})
+
+window.addEventListener('keydown', e => {
+    if (e.keyCode == 32) {
+        let hex = '#'
+        for (i = 0; i < 6; i++) {
+            let randomColor = Math.floor(Math.random() * hexColor.length);
+            hex += hexColor[randomColor];
+            pelette2.style.backgroundColor = hex;
+            colorDisplay2.innerHTML = hex;
         }
     }
 })
